0319923cb95a6c3bb571f711bb80eebefd963309
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-01-22 03:21:32 +0800

    create model admin for buzz

--- buzz/admin.py
+++ buzz/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Buzz
+
+@admin.register(Buzz)
+class BuzzAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'status', 'publish', 'updated']
+    list_filter = ['status', 'slug', 'publish']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS
