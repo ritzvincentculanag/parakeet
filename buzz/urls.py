8b50ffe34fc398a5e4a8c6b0146981ae0e557efd
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-01-22 03:33:58 +0800

    add initial view for buzzes

--- buzz/urls.py
+++ buzz/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
+from . import views
 
 urlpatterns = [
-
+    path(
+        route='',
+        view=views.buzz_list,
+        name='buzz_list'
+    )
 ]
--- buzz/views.py
+++ buzz/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from buzz.models import Buzz
+
+
+def buzz_list(request):
+    buzzes = Buzz.published.all()
+
+    return render(
+        request=request,
+        template_name='buzz/list.html',
+        context={ 'buzzes': buzzes }
+    )
