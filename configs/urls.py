6d9f2ed47a097086262cbeddc19679bca68795e8
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-01-22 03:32:31 +0800

    update route name to buzz

--- configs/urls.py
+++ configs/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
-        route='',
+        route='buzz/',
         view=include(('buzz.urls', 'buzz'), namespace='buzz')
     )
 ]
