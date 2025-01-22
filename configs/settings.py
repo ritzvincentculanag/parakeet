7904dac6a76b6164a2b076501a66661b1c3dde74
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-01-23 04:03:49 +0800

    add crispy forms bulma

--- configs/settings.py
+++ configs/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
+    'crispy_forms',
+    'crispy_bulma',
     'buzz.apps.BuzzConfig',
 ]
 
@@ -123,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy Forms
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ('bulma',)
+
+CRISPY_TEMPLATE_PACK = 'bulma'
