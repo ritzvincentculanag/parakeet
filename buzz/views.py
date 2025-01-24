75f10ea80e7bf08dd2104290e35b6820f8156434
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-01-24 13:55:16 +0800

    add initial messages for crud

--- buzz/views.py
+++ buzz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+from django.contrib import messages
 
 from buzz.models import Buzz
 from buzz.forms import BuzzCreateForm
@@ -24,6 +25,11 @@ def buzz_create(request):
             buzz = form.save(commit=False)
             buzz.author = request.user
             buzz.save()
+
+            messages.success(
+                request=request,
+                message='Buzz created successfully!'
+            )
             
             return redirect(to=reverse('buzz:buzz_list'))
 
@@ -38,6 +44,11 @@ def buzz_delete(request, id):
 
     if request.method == 'POST':
         buzz.delete()
+
+        messages.success(
+            request=request,
+            message='Buzz deleted successfully!'
+        )
 
         return redirect(reverse('buzz:buzz_list'))
 
