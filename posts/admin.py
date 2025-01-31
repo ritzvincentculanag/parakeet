47494686d0122c4b8d17de299d0effe1a226fb7e
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-01-31 12:43:39 +0800

    Register models to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['author','content','created','status']
+    list_filter = ['author','status']
+    search_fields = ['author']
+    ordering = ['status','created']
+    date_hierarchy = 'created'
+    show_facets = admin.ShowFacets.ALWAYS
