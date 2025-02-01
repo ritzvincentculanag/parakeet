bea2ea918447d637dd29d3feb205740d7992ea7d
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-02-01 18:02:41 +0800

    Add initial migrations

--- posts/admin.py
+++ posts/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Like
 
 @admin.register(Post)
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['author','content','created','status']
     list_filter = ['author','status']
-    search_fields = ['author']
+    search_fields = ['content']
     ordering = ['status','created']
     date_hierarchy = 'created'
     show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Like)
+class LikeModelAdmin(admin.ModelAdmin):
+    list_display = ['post']
+    search_fields = ['post__content']
+    date_hierarchy = 'created'
