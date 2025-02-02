5a44a87aed09c646a46ebac6a470a4e82422f3d9
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-02-02 17:41:11 +0800

    Add box icons moduel

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+const chirpTextArea = document.querySelector('.form__post-content')
+const chirpCounter = document.querySelector('.form__post-counter');
+
+chirpTextArea.addEventListener('input', (e) => {
+	const characterCount = e.target.value.length;
+	chirpCounter.style.color = characterCount === 250 ? "red" : "var(--clr-accent)";
+	chirpCounter.innerHTML = characterCount;
+})
