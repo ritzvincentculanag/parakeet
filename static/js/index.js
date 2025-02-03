1eb3d04438eed8c43bad520e038e9f31b26244e7
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-02-04 03:00:59 +0800

    Reformat code

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ const chirpTextArea = document.querySelector('.form__post-content')
 const chirpCounter = document.querySelector('.form__post-counter');
 
 chirpTextArea.addEventListener('input', (e) => {
-  const characterCount = e.target.value.length;
-  chirpCounter.style.color = characterCount === 250 ? "red" : "var(--clr-accent)";
-  chirpCounter.innerHTML = characterCount;
+	const characterCount = e.target.value.length;
+	chirpCounter.style.color = characterCount === 250 ? "red" : "var(--clr-accent)";
+	chirpCounter.innerHTML = characterCount;
 })
