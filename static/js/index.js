b4c9029c5a21bbb48e4967f97c00a87031854a1d
Arvee <ritzvincentvergaraculanag@gmail.com>
2025-02-05 03:04:08 +0800

    #32: Add style to star if user liked the post at load

--- static/js/index.js
+++ static/js/index.js
@@ -6,3 +6,19 @@ chirpTextArea.addEventListener('input', (e) => {
   chirpCounter.style.color = characterCount === 250 ? "red" : "var(--clr-accent)";
   chirpCounter.innerHTML = characterCount;
 })
+
+
+const posts = document.getElementsByClassName('post')
+
+Array.from(posts).forEach(post => {
+  if (post.dataset.liked === "true") {
+    const postEngagement = post.children[1].children[2].children[0];
+    const postLikeButton = postEngagement.children[1]
+    const postLikeCounter = postEngagement.children[2]
+
+    postLikeButton.children[0].classList.remove('bx-star')
+    postLikeButton.children[0].classList.add('bxs-star')
+    postLikeButton.children[0].style.color = "var(--clr-accent)"
+    postLikeCounter.style.color = "var(--clr-accent)"
+  }
+})
